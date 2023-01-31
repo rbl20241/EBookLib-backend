bc223d2d6e158608e3a8428df3a6cd020714242c
Rene <rene.blom@rabobank.nl>
2023-01-31 12:29:01 +0100

    Converted to Spring Boot 3 (part 4)

--- src/main/java/rb/ebooklib/configuration/SecurityConfig.java
+++ src/main/java/rb/ebooklib/configuration/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import rb.ebooklib.security.CustomUserDetailsService;
 import rb.ebooklib.security.RestAuthenticationEntryPoint;
 import rb.ebooklib.security.TokenAuthenticationFilter;
@@ -16,40 +17,6 @@ import rb.ebooklib.security.TokenAuthenticationFilter;
 @Configuration
 class SecurityConfig {
 
-//    @Autowired
-//    private CustomUserDetailsService customUserDetailsService;
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-        return new CustomUserDetailsService(bCryptPasswordEncoder);
-    }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-//        return customUserDetailsService;
-//    }
-
-//    @Bean
-//    public AuthorizationManager<MethodInvocation> authorizationManager() {
-//        return new CustomAuthorizationManager<>();
-//    }
-//
-//    @Bean
-//    @Role(ROLE_INFRASTRUCTURE)
-//    public Advisor authorizationManagerBeforeMethodInterception(AuthorizationManager<MethodInvocation> authorizationManager) {
-//        JdkRegexpMethodPointcut pattern = new JdkRegexpMethodPointcut();
-//        pattern.setPattern("com.baeldung.enablemethodsecurity.services.*");
-//        return new AuthorizationManagerBeforeMethodInterceptor(pattern, authorizationManager);
-//    }
-//
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().formLogin().disable().httpBasic().disable()
@@ -57,64 +24,120 @@ class SecurityConfig {
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint())
                 .and()
                 .authorizeHttpRequests()
+                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/auth/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilter(tokenAuthenticationFilter());
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-
-
-
-//    @Override
-//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//        authenticationManagerBuilder
-//                .userDetailsService(customUserDetailsService)
-//                .passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-
-    @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter();
     }
+
+
+////    @Autowired
+////    private CustomUserDetailsService customUserDetailsService;
 //
-//    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+//        return new CustomUserDetailsService(bCryptPasswordEncoder);
 //    }
 //
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
+////    @Bean
+////    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+////        return customUserDetailsService;
+////    }
+//
+////    @Bean
+////    public AuthorizationManager<MethodInvocation> authorizationManager() {
+////        return new CustomAuthorizationManager<>();
+////    }
+////
+////    @Bean
+////    @Role(ROLE_INFRASTRUCTURE)
+////    public Advisor authorizationManagerBeforeMethodInterception(AuthorizationManager<MethodInvocation> authorizationManager) {
+////        JdkRegexpMethodPointcut pattern = new JdkRegexpMethodPointcut();
+////        pattern.setPattern("com.baeldung.enablemethodsecurity.services.*");
+////        return new AuthorizationManagerBeforeMethodInterceptor(pattern, authorizationManager);
+////    }
+////
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.cors().and().csrf().disable().formLogin().disable().httpBasic().disable()
 //                .exceptionHandling()
 //                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
 //                .and()
-//                .authorizeRequests()
-//                .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/auth/**")
-//                .permitAll()
+//                .authorizeHttpRequests()
 //                .anyRequest()
 //                .authenticated()
 //                .and()
 //                .sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //
-//        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//        http.addFilter(tokenAuthenticationFilter());
+//
+//        return http.build();
 //    }
+//
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+//
+//
+//
+//
+//
+////    @Override
+////    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+////        authenticationManagerBuilder
+////                .userDetailsService(customUserDetailsService)
+////                .passwordEncoder(passwordEncoder());
+////    }
+////
+//
+//    @Bean
+//    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+//        return new TokenAuthenticationFilter();
+//    }
+////
+////    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+////    @Override
+////    public AuthenticationManager authenticationManagerBean() throws Exception {
+////        return super.authenticationManagerBean();
+////    }
+////
+////    @Override
+////    protected void configure(HttpSecurity http) throws Exception {
+////        http.cors().and().csrf().disable().formLogin().disable().httpBasic().disable()
+////                .exceptionHandling()
+////                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+////                .and()
+////                .authorizeRequests()
+////                .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**", "/auth/**")
+////                .permitAll()
+////                .anyRequest()
+////                .authenticated()
+////                .and()
+////                .sessionManagement()
+////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////
+////        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+////    }
 }
--- src/main/java/rb/ebooklib/security/CustomUserDetailsService.java
+++ src/main/java/rb/ebooklib/security/CustomUserDetailsService.java
@@ -5,16 +5,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import rb.ebooklib.model.User;
 import rb.ebooklib.persistence.UserRepository;
-import rb.ebooklib.user.SecurityUser;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,28 +15,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
-    private final Map<String, SecurityUser> userMap = new HashMap<>();
-
-    public CustomUserDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-        userMap.put("user", createUser("user", bCryptPasswordEncoder.encode("userPass"), false, "USER"));
-        userMap.put("admin", createUser("admin", bCryptPasswordEncoder.encode("adminPass"), true, "ADMIN", "USER"));
-    }
-
     @Override
     public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
         final User user = userRepository.findByUsernameIgnoreCase(username)
                 .orElseThrow(() -> new IllegalArgumentException("Gebruiker " + username + " bestaat niet"));
         return UserPrincipal.create(user);
     }
-
-    private SecurityUser createUser(String userName, String password, boolean withRestrictedPolicy, String... role) {
-        return SecurityUser.builder()
-                .withUserName(userName)
-                .withPassword(password)
-                .withGrantedAuthorityList(Arrays.stream(role)
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList()))
-                .withAccessToRestrictedPolicy(withRestrictedPolicy);
-    }
-
 }
